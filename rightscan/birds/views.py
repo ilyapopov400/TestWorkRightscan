4f88bfdcf6fbbe7ef6b6bc1fe5754efe3363d378
ilya_popov <il_pop@mail.ru>
2024-03-05 14:59:42 +0300

    base.css, base.html, index.html

--- rightscan/birds/views.py
+++ rightscan/birds/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+
+class Index(TemplateView):
+    '''
+    стартовая страница приложения birds
+    '''
+    template_name = 'birds/index.html'
