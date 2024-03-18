095590cf4f0b7d64153a29feedb8e0dc4fb407a2
ilya_popov <il_pop@mail.ru>
2024-03-18 14:03:29 +0300

    class OneBird, проставление лайков, удалить запись нельзя

--- rightscan/birds/urls.py
+++ rightscan/birds/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),  # просмотр главной страницы
     path('birds/', views.BirdsShow.as_view(), name='show-all-birds'),  # просмотр всех птиц
     path('birds/<int:pk>', views.OneBird.as_view(), name='one-bird-detail'),  # просмотр одной птицы
-    path('birds/create', views.TouchBird.as_view(), name='create-bird'),  # создание новой карточки для птицы
+    path('birds/create', views.CreateBird.as_view(), name='create-bird'),  # создание новой карточки для птицы
 ]
--- rightscan/birds/views.py
+++ rightscan/birds/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView, DeleteView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views import View
 from django.contrib import messages
 
 from . import models
@@ -27,29 +28,38 @@ class BirdsShow(ListView):
     context_object_name = 'birds'
 
 
-class OneBird(DetailView, DeleteView):
+class OneBird(View):
     """
-    Показ одной птицы и возможность ее удалить
+    Показ одной птицы
+    Проставление лайка птице
+    и возможность ее удалить - не реализованно
     """
-    template_name = 'birds/bird-detail.html'
-    model = models.Birds
-    context_object_name = 'bird'
-    success_url = reverse_lazy('birds:index')
+
+    def get(self, request, pk=None):
+        bird = models.Birds.objects.get(id=pk)
+        template_name = "birds/bird-detail.html"
+        context = {"bird": bird}
+        return render(request=request, template_name=template_name, context=context)
+
+    def post(self, request, pk=None):
+        bird = models.Birds.objects.get(id=pk)
+        template_name = "birds/bird-detail.html"
+        context = {"bird": bird}
+        like = models.Birds.objects.get(id=pk).like + 1
+        bird.like = like
+        bird.save()
+        return render(request=request, template_name=template_name, context=context)
 
 
-class TouchBird(CreateView):
+class CreateBird(CreateView):
     """
     Создание новой карточки птицы
     """
     form_class = forms.FormBirds
     template_name = "birds/create-bird.html"
-    # model = models.Birds
-    # fields = ["name", "color", "image" ]
+    model = models.Birds
+    # fields = ["name", "color", "image"]
     success_url = reverse_lazy('birds:index')
-
-    def form_valid(self, form):
-        form.save()
-        return super(TouchBird, self).form_valid(form)
 
     # def form_valid(self, form):
     #     form.instance.user = self.request.user
